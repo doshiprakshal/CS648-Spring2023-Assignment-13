5e9dd4e96976f9a592a4e2da5077ecb7823d9449
Prakshal Doshi <30745339+doshiprakshal@users.noreply.github.com>
2023-04-30 18:00:18 -0700

    Initial commit

---
+++ module01/public/employees.js
@@ -0,0 +1,15 @@
+let jerome = "Jerome Warfield";
+let emily = "Emily Murillo";
+let aiza = "Aiza Baybay";
+let chris = "Chris Shookup";
+let elizabeth = "Elizabeth Waterman";
+let raj = "Raj Tiwari";
+let chetan = "Chetan Nainu";
+let jigs = "Jigs Patel";
+const element = /*#__PURE__*/React.createElement("ul", {
+  style: {
+    color: "blue",
+    fontSize: "24px"
+  }
+}, /*#__PURE__*/React.createElement("li", null, jerome), /*#__PURE__*/React.createElement("li", null, emily), /*#__PURE__*/React.createElement("li", null, aiza), /*#__PURE__*/React.createElement("li", null, chris), /*#__PURE__*/React.createElement("li", null, elizabeth), /*#__PURE__*/React.createElement("li", null, raj), /*#__PURE__*/React.createElement("li", null, chetan), /*#__PURE__*/React.createElement("li", null, jigs));
+ReactDOM.render(element, document.getElementById("content"));
---
+++ module01/src/employees.jsx
@@ -0,0 +1,21 @@
+let jerome = "Jerome Warfield";
+let emily = "Emily Murillo";
+let aiza = "Aiza Baybay";
+let chris = "Chris Shookup";
+let elizabeth = "Elizabeth Waterman";
+let raj = "Raj Tiwari";
+let chetan = "Chetan Nainu";
+let jigs = "Jigs Patel";
+const element = (
+  <ul style={{ color: "blue", fontSize: "24px" }}>
+    <li>{jerome}</li>
+    <li>{emily}</li>
+    <li>{aiza}</li>
+    <li>{chris}</li>
+    <li>{elizabeth}</li>
+    <li>{raj}</li>
+    <li>{chetan}</li>
+    <li>{jigs}</li>
+  </ul>
+);
+ReactDOM.render(element, document.getElementById("content"));
